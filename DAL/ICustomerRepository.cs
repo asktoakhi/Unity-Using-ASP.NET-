24a134630246dc5f48bc64be06249aedbb591642
asktoakhi <akhilesh.kushwaha356@gmail.com>
2018-12-14 11:27:28 +0530

    Here, I am going to explain how to implement dependency injection in MVC project, including separate layers for getting data through Services and Repositories.

---
+++ DAL/ICustomerRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using WebX.Common;
+
+namespace DAL
+{
+    public interface ICustomerRepository
+    {
+
+       // DataTable Getdata();
+       // object GetCustomer();
+
+        List<Customer> GetCustomers();
+
+    }
+}
---
+++ Services/CustomerService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL;
+namespace Services
+{
+    public class iCustomerService : ICustomerService
+    {
+        private ICustomerRepository _iCustomerRepository;
+
+        public iCustomerService(ICustomerRepository iCustomerRepository)
+        {
+            _iCustomerRepository = iCustomerRepository;
+        }
+
+        // public List<Customer> GetCustomers()
+        //{
+        //    return _iCustomerRepository.GetCustomers();
+        //}
+
+     public   List<Customer> GetCustomers()
+        {
+            CustomerRepository customerRepository = new CustomerRepository();
+            return customerRepository.GetCustomers();
+        }
+    }
+}
